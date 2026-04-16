2a3b209c566588e136546b8e11e236f24b0846cf
Stevnbak <kasper@stevnbak.dk>
2026-04-16 19:15:59 +0200

    Max notifications + vis under navbar

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -13,9 +13,13 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faRightFromBracket, faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { } from '@fortawesome/free-regular-svg-icons'
 import { } from '@fortawesome/free-brands-svg-icons'
+import Vue3Toastify from 'vue3-toastify'
 library.add([faRightFromBracket, faPenToSquare, faTrash])
 
 createApp(App)
     .use(router)
+    .use(Vue3Toastify, {
+        limit: 1
+    })
     .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
