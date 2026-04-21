f8ca4223d8b3674848b8c737afb69c7e827bd778
Rasmus1211 <rasmusen@hotmail.dk>
2026-04-21 22:51:19 +0200

    updated initial test set up to hit buttons instead of links

--- frontend/tests/auth/login.spec.ts
+++ frontend/tests/auth/login.spec.ts
@@ -3,5 +3,6 @@ import { expect } from '@playwright/test';
 
 test('global setup logs user in successfully', async ({ page }) => {
   await page.goto('/');
-  await expect(page.getByRole('link', { name: /log out/i })).toBeVisible();
+  await page.getByRole('button', { name: /Account/i }).click();
+  await expect(page.getByRole('button', { name: /log out/i })).toBeVisible();
 });
